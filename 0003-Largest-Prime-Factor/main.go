ef74ae13184ff0c30dbfb5f9b9a8fe82e2f74403
Amalio769 <amalio.rete-campos@airbus.com>
2024-10-26 08:44:10 +0200

    go 0003

--- 0003-Largest-Prime-Factor/main.go
+++ 0003-Largest-Prime-Factor/main.go
@@ -2,3 +2,39 @@
 // What is the largest prime factor of the number 600851475143 ?
 // Answer:  6857
 // Completed on Sun, 14 Aug 2022, 22:37
+
+// algorithm FindPrimesEratosthenes(n):
+//     // INPUT
+//     //    n = an arbitrary number
+//     // OUTPUT
+//     //    prime numbers smaller than n
+//
+//     A <- an array of size n with boolean values set to true
+//
+//     for i <- 2 to sqrt(n):
+//         if A[i] is true:
+//             j <- i^2
+//             while j <= n:
+//                 A[j] <- false
+//                 j <- j + i
+//
+//     return the indices of A corresponding to true
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(PrimeFactor(600851475143))
+}
+
+func FindPrimesEratosthenes(n int64) []int64 {
+	A := make([]bool, n+1)
+	for k := range n + 1 {
+		if k == 0 {
+			A[k] = false
+		} else {
+			A[k] = true
+		}
+	}
+}
