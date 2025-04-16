c6299c144d1f2a0d34d40e4f5c91bf82f234f21d
Amalio769 <amalio.rete@gmail.com>
2025-04-16 23:29:36 +0200

    0005 Go OK

--- 0005 Smallest multiple/main.go
+++ 0005 Smallest multiple/main.go
@@ -20,28 +20,53 @@ func main() {
 	// 1. Find the prime factors of each number from 1 to 20
 	n := 20
 	factors := make(map[int]int)
+	fmt.Println("Factors: ", factors)
 	for i := 2; i <= n; i++ {
-		num := i
-		for j := 2; j <= num; j++ {
-			if num%j == 0 {
-				factors[j]++
-				num /= j
-				j--
+		if IsPrimeNumber(i) {
+			factors[i] = 1
+			continue
+		}
+	}
+	fmt.Println("Factors: ", factors)
+	// 2. Find the maximum exponent of each prime factor
+	for num := 2; num < n; num++ {
+		for k, v := range factors {
+			if num%k == 0 {
+				max_exp := MaxExponentDiv(num, k)
+				if max_exp > v {
+					factors[k] = max_exp
+				}
 			}
 		}
 	}
-	// 2. Find the maximum exponent of each prime factor
-	maxFactors := make(map[int]int)
-	for k, v := range factors {
-		if maxFactors[k] < v {
-			maxFactors[k] = v
-		}
-	}
+	fmt.Println("Factors updated with maximum exponent: ", factors)
 	// 3. Multiply the prime factors raised to their maximum exponents
 	result := big.NewInt(1)
-	for k, v := range maxFactors {
+	for k, v := range factors {
 		result.Mul(result, big.NewInt(int64(math.Pow(float64(k), float64(v)))))
 	}
 	fmt.Println("Answer: ", result)
 	fmt.Println("Execution time: ", time.Since(start))
+}
+
+func IsPrimeNumber(n int) bool {
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func MaxExponentDiv(num int, div int) int {
+	if num%div == 0 {
+		max_exp := 1
+		for i := 2; int(math.Pow(float64(div), float64(i))) <= num; i++ {
+			if num%int(math.Pow(float64(div), float64(i))) == 0 {
+				max_exp++
+			}
+		}
+		return max_exp
+	}
+	return 0
 }
