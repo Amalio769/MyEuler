f0ed180a941cd04e09b1519c31d2750b263c5f37
Amalio769 <amalio.rete@gmail.com>
2025-04-17 19:52:15 +0200

    0007 go completed

---
+++ 0007 10001st prime/go.mod
@@ -0,0 +1,3 @@
+module example.com/myeuler/myeuler0007
+
+go 1.24.2
--- 0007 10001st prime/main.go
+++ 0007 10001st prime/main.go
@@ -3,6 +3,8 @@
 // What is the 10 001st prime number?
 // Answer:  104743
 // Completed on Mon, 15 Aug 2022, 15:54
+// Using the function IsPrimeFactor the computational time is around 4.533 sec
+// Using the function NextPrimeFactor the computational time is around 0.635 sec
 package main
 
 import (
@@ -12,18 +14,29 @@ import (
 
 func main() {
 	start := time.Now()
-	number := 2
+
+	// number := 2
+	// var list_prime []int
+	// for {
+	// 	if IsPrimeFactor(number) {
+	// 		list_prime = append(list_prime, number)
+	// 		if len(list_prime) == 10001 {
+	// 			break
+	// 		}
+	// 	}
+	// 	number++
+	// }
+	// fmt.Println(list_prime[len(list_prime)-1])
+
 	var list_prime []int
 	for {
-		if IsPrimeFactor(number) {
-			list_prime = append(list_prime, number)
-			number++
-			if len(list_prime) == 10001 {
-				break
-			}
+		list_prime = append(list_prime, NextPrimeFactor(list_prime))
+		if len(list_prime) == 10001 {
+			break
 		}
 	}
 	fmt.Println(list_prime[len(list_prime)-1])
+
 	fmt.Println("Tiempo de ejecución: ", time.Since(start))
 }
 
@@ -34,4 +47,22 @@ func IsPrimeFactor(num int) bool {
 		}
 	}
 	return true
+}
+
+func NextPrimeFactor(list_prime_factor []int) int {
+	if len(list_prime_factor) == 0 {
+		return 2
+	}
+	for num := list_prime_factor[len(list_prime_factor)-1] + 1; ; num++ {
+		num_is_prime_factor := true
+		for _, pf := range list_prime_factor {
+			if num%pf == 0 {
+				num_is_prime_factor = false
+				break
+			}
+		}
+		if num_is_prime_factor {
+			return num
+		}
+	}
 }
