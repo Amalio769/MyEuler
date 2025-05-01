9205a58138b33719bd613833997cfe51f59871c8
Amalio769 <amalio.rete@gmail.com>
2025-05-01 07:32:46 +0200

    0016 go done

--- 0016 Power digit sum/main.go
+++ 0016 Power digit sum/main.go
@@ -13,14 +13,12 @@ import (
 func main() {
 	start := time.Now()
 	base := big.NewInt(2)
-	exponent := big.NewInt(15)
+	exponent := big.NewInt(1000)
 	result := new(big.Int).Exp(base, exponent, nil)
 	strResult := result.String()
 	sum_strResult := 0
-	for i, n := range strResult {
-		sum_strResult += int(n)
-		fmt.Println(i, n)
-
+	for _, n := range strResult {
+		sum_strResult += int(n - '0')
 	}
 	fmt.Println(sum_strResult)
 	fmt.Println("Computational time: ", time.Since(start))
