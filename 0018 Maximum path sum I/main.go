f18c6d84e1802a3e4687f782a09bf943cd623fe3
Amalio769 <amalio.rete@gmail.com>
2025-05-02 08:33:44 +0200

    0018 Go done

--- 0018 Maximum path sum I/main.go
+++ 0018 Maximum path sum I/main.go
@@ -45,5 +45,62 @@ import (
 func main() {
 	start := time.Now()
 
+	triangle := [][]int{
+		{75},
+		{95, 64},
+		{17, 47, 82},
+		{18, 35, 87, 10},
+		{20, 4, 82, 47, 65},
+		{19, 1, 23, 75, 03, 34},
+		{88, 2, 77, 73, 7, 63, 67},
+		{99, 65, 4, 28, 6, 16, 70, 92},
+		{41, 41, 26, 56, 83, 40, 80, 70, 33},
+		{41, 48, 72, 33, 47, 32, 37, 16, 94, 29},
+		{53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14},
+		{70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57},
+		{91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48},
+		{63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
+		{04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23},
+	}
+	// check if triangle is correct
+	if !checkTriangleIsOK(triangle) {
+		fmt.Println("The triangle is not correct.")
+		return
+	}
+	fmt.Println("Resultado:", maximumPathSumTriangle(triangle))
 	fmt.Println("Computational time: ", time.Since(start))
+}
+
+func checkTriangleIsOK(triangle [][]int) bool {
+	for i, row := range triangle {
+		if len(row) != i+1 {
+			return false
+		}
+	}
+	return true
+}
+
+func maximumPathSumTriangle(triangle [][]int) int {
+	result := triangle
+	for {
+		if lt := len(result); lt > 1 {
+			result_sum := []int{}
+			pos_row_sum := lt - 2
+			len_row_sum := len(result[lt-2])
+			for i := 0; i < len_row_sum; i++ {
+				result_sum = append(result_sum, result[pos_row_sum][i]+Max2Values(result[pos_row_sum+1][i], result[pos_row_sum+1][i+1]))
+			}
+			result = append(result[0:lt-2], result_sum)
+		} else {
+			break
+		}
+	}
+	return result[0][0]
+}
+
+func Max2Values(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
