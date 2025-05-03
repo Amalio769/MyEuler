e071ecff539b5b29d956e74e5a149dea9be23222
Amalio769 <amalio.rete@gmail.com>
2025-05-03 21:16:19 +0200

    created factorial function for big.Int

---
+++ 0020 Factorial digit sum/go.mod
@@ -0,0 +1,3 @@
+module example.com/myeuler/0020
+
+go 1.24.2
--- 0020 Factorial digit sum/main.go
+++ 0020 Factorial digit sum/main.go
@@ -9,11 +9,27 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-
+	num := big.NewInt(100)
+	fmt.Println(factorial(num))
 	fmt.Println("Computational time: ", time.Since(start))
+}
+
+// factorial  Función Factorial recursiva
+func factorial(n *big.Int) *big.Int {
+	// Caso base: si n == 1, retorna 1
+	if n.Cmp(big.NewInt(1)) == 0 {
+		return big.NewInt(1)
+	}
+
+	// Crear una copia de n para evitar modificar el valor original
+	nMinusOne := new(big.Int).Sub(n, big.NewInt(1))
+
+	// Calcular factorial recursivamente
+	return new(big.Int).Mul(n, factorial(nMinusOne))
 }
