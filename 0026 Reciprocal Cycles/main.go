db53cdc6481f3ddb23758ce9773a5b1d1d9c0358
Amalio769 <amalio.rete@gmail.com>
2025-05-13 21:15:12 +0200

    solved 0026 go

--- 0026 Reciprocal Cycles/main.go
+++ 0026 Reciprocal Cycles/main.go
@@ -12,33 +12,69 @@
 // Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
 //
 // Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
-// Answer:
+// Answer: 983
 package main
 
 import (
+	"errors"
 	"fmt"
-	"regexp"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-
-	// re := regexp.MustCompile(`(\d+?)+`)
-	// one := big.NewFloat(1)
-	// for i := 2; i <= 100; i++ {
-	// 	denominator := big.NewFloat(float64(i)).SetPrec(9000)
-	// 	result := new(big.Float).Quo(one, denominator)
-	// 	result_text := result.Text('f', 130)
-	// 	match := re.FindStringSubmatch(result_text)
-	// 	if len(match) > 1 {
-	// 		fmt.Printf("Para 1/%d : %s", i, match)
-	// 	}
-	// }
-	number := "0.0114942528735632183908045977011494252873563218390804597701149425287356321839080459770114942528735632183908045977"
-	re := regexp.MustCompile(`([0-9]+?)+`)
-	match := re.FindStringSubmatch(number)
-	fmt.Println(match)
+	max_recurring_cicle := 0
+	max_number := 0
+	for i := 2; i < 1000; i++ {
+		result := LenRecurringCycleOneDividedNum(i)
+		if result > max_recurring_cicle {
+			max_recurring_cicle = result
+			max_number = i
+		}
+	}
+	fmt.Printf("For the number 1/%d, the result has a recurring cicle decimals of %d\n", max_number, max_recurring_cicle)
 
 	fmt.Println("Computational time: ", time.Since(start))
+}
+
+func LenRecurringCycleOneDividedNum(num int) int {
+	list_remainder := []int{}
+	next_num := 1
+	for {
+		remainder := next_num % num
+		if IsInSlice(list_remainder, remainder) || remainder == 0 {
+			list_remainder = append(list_remainder, remainder)
+			break
+		} else {
+			list_remainder = append(list_remainder, remainder)
+			next_num = remainder * 10
+		}
+	}
+	// fmt.Println(list_remainder)
+	value := list_remainder[len(list_remainder)-1]
+	first_ocurrence_value, err := FindIndex(list_remainder, value)
+	if err != nil {
+		return 0
+	}
+	last_ocurrence_value := len(list_remainder) - 1
+
+	return last_ocurrence_value - first_ocurrence_value
+}
+
+func IsInSlice(slice []int, num int) bool {
+	for _, value := range slice {
+		if value == num {
+			return true
+		}
+	}
+	return false
+}
+
+func FindIndex(slice []int, num int) (int, error) {
+	for idx, value := range slice {
+		if value == num {
+			return idx, nil
+		}
+	}
+	return -1, errors.New("número no encontrado en el slice")
 }
