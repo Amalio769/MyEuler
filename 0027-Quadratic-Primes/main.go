a4d420e5a1f4adc5916d499d919e41f22a1a22cb
Amalio <56455295+Amalio769@users.noreply.github.com>
2025-12-03 17:55:40 +0100

    enunciado 0027 Quadratic Primes

---
+++ 0027-Quadratic-Primes/main.go
@@ -0,0 +1,18 @@
+// Euler discovered the remarkable quadratic formula:
+// 𝑛2 +𝑛 +41
+// It turns out that the formula will produce 40 primes for the consecutive integer values 0 ≤𝑛 ≤39.
+// However, when 𝑛 =40,402 +40 +41 =40⁢(40 +1) +41 is divisible by 41, and certainly when 𝑛 =41,412 +41 +41 is
+// clearly divisible by 41.
+//
+// The incredible formula 𝑛2 −79⁢𝑛 +1601 was discovered, which produces 80 primes for the consecutive values 0 ≤𝑛 ≤79.
+// The product of the coefficients, −79 and 1601, is −126479.
+//
+// Considering quadratics of the form:
+//
+// 𝑛2 +𝑎⁢𝑛 +𝑏, where |𝑎| <1000 and |𝑏| ≤1000
+//
+// where |𝑛| is the modulus/absolute value of 𝑛
+// e.g. |11| =11 and |−4| =4
+// Find the product of the coefficients, 𝑎 and 𝑏, for the quadratic expression that produces the maximum number of primes
+// for consecutive values of 𝑛, starting with 𝑛 =0.
+package main
